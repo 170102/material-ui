558abf690a65f5e6d909957f5fe73ad730658be5
170102 <KR170102@euro.ngc.com>
2017-02-08 13:40:01 +0000

    [Menu] add roles for to reflect presentation role of wrapper and menu role of list

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -556,11 +556,13 @@ class Menu extends Component {
           onWheel={this.handleOnWheel}
           style={prepareStyles(mergedRootStyles)}
           ref="scrollContainer"
+          role="presentation"
         >
           <List
             {...other}
             ref="list"
             style={mergedListStyles}
+            role="menu"
           >
             {newChildren}
           </List>
